3eda6d8b2dcc05ff8b32b08ee17664909d3b2a83
Stephen Moore <stephenmoore33@outlook.com>
2021-04-29 13:05:33 -0400

    added variables for time of day

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,34 @@
+$(document).ready(function() {
+// applies js once page is fully loaded
+
+
+if(localStorage.getItem("locStorage")){
+    times = JSON.parse(localStorage.getItem("locStorage"));
+}
+
+var timetable = [
+{hour: 09, eventInput: " " }, // 9 am
+{hour: 10, eventInput: " " }, // 10 am
+{hour: 11, eventInput: " " }, // 11 am
+{hour: 12, eventInput: " " }, // 12 pm noon
+{hour: 13, eventInput: " " }, // 1 pm
+{hour: 14, eventInput: " " }, // 2 pm
+{hour: 15, eventInput: " " }, // 3 pm
+{hour: 16, eventInput: " " }, // 4 pm
+{hour: 17, eventInput: " " }, // 5 pm
+]
+
+if(localStorage.getItem("savedInput")) {
+    timetable = JSON.parse(localStorage.getItem("savedInput"));
+}
+
+// applies current date / time to currTime class
 var elements = document.getElementsByClassName("currTime");
 for (var i = 0; i < elements.length; i++) {
-    elements[ i ].innerHTML = moment().format("MM/DD/YYYY HH:MM A");
-} // applies current date / time to currTime class
+    elements[ i ].innerHTML = moment().format("MM/DD/YYYY HH:MM A")
+}
+
+
+
+
+});
